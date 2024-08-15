63ee51d317b10a468a9ad12977a9bba4c2c1fe51
Sanjiv Thapa <154358088+sanjivthapasvt@users.noreply.github.com>
2024-08-15 21:23:25 +0545

    Update rock paper scissor.py

--- rock paper scissor.py
+++ rock paper scissor.py
@@ -2,44 +2,26 @@ from random import randint
 
 choice = ["Rock", "Paper", "Scissor"]
 computer = choice[randint(0, 2)]
-player = False
+player = True
 
-while not player:
+while player:
     check = input('Do you want to play?(Y/N) ').title()
     if check == "Y":
         player = input('Enter "Rock" "Paper" or "Scissor": ').title()
+        print(f"You picked {player} and Computer picked {computer}")
         if player == computer:
-            print(f"You picked {player} and Computer picked {computer}")
             print("Tie!!")
 
-        elif player == "Rock":
-            if computer == "Paper":
-                print(f"You picked {player} and Computer picked {computer}")
-                print("You lost!!")
-            elif computer == "Scissor":
-                print(f"You picked {player} and Computer picked {computer}")
-                print("You Win!!")
+        elif player == "Rock" and computer == "Paper" or player == "Paper" and computer == "Scissor" or player == "Scissor" and computer == "Rock":
+            print("You lost!!")
 
-        elif player == "Paper":
-            if computer == "Rock":
-                print(f"You picked {player} and Computer picked {computer}")
-                print("You win!!")
-            elif computer == "Scissor":
-                print(f"You picked {player} and Computer picked {computer}")
-                print("You lost!!")
-
-        elif player == "Scissor":
-            if computer == "Paper":
-                print(f"You picked {player} and Computer picked {computer}")
-                print("You win!!")
-            elif computer == "Rock":
-                print(f"You picked {player} and Computer picked {computer}")
-                print("You lost!!")
+        elif player == "Rock" and computer == "Scissor" or player == "Paper" and computer == "Rock" or player == "Scissor" and computer == "Paper":
+            print("You win!!")
 
         else:
             print("Please enter valid input!!")
 
-        player = False
+        player = True
         computer = choice[randint(0, 2)]
 
     else:
